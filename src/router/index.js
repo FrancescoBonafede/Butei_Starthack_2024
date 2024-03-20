7ac35aac5060a4946c8742b8a8cf0b507f63506a
Frameck <fra9879@gmail.com>
2024-03-20 23:28:01 +0100

    moved to home

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,6 @@ const router = createRouter({
 				{
 					path: '',
 					name: 'home',
-					component: () => import('@/pages/home/HomeView.vue'),
-				},
-				{
-					path: '/earth',
-					name: 'home',
 					component: () => import('@/pages/home/EarthView.vue'),
 				},
 				{
